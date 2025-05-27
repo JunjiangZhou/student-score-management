2ace8c0404f1453f59f09f884eda7a512eb3c2d5
zhoujunjiang <hchen5273@gmail.com>
2025-05-27 16:26:37 +0800

    1

---
+++ src/main/java/org/zhoujunjiang/grade/config/AppConfig.java
@@ -0,0 +1,60 @@
+package org.zhoujunjiang.grade.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@Configuration
+@ComponentScan(basePackages = {"org.zhoujunjiang.grade.service"})
+@EnableTransactionManagement
+public class AppConfig {
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/student_grade_system?useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Shanghai");
+        ds.setUsername("root");
+        ds.setPassword("Mysql4mypj");
+        ds.setMaxActive(20);
+        ds.setMaxIdle(5);
+        return ds;
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory() {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setConfigLocation(new org.springframework.core.io.ClassPathResource("mybatis-config.xml"));
+        try {
+            factoryBean.setMapperLocations(
+                    new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*.xml")
+            );
+        } catch (IOException e) {
+            throw new RuntimeException("无法加载 Mapper XML 文件", e);
+        }
+        return factoryBean;
+    }
+
+    // 注意：MapperScannerConfigurer 不能写 @Bean 时返回接口类，要用这个方式配置
+    @Bean
+    public static MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer configurer = new MapperScannerConfigurer();
+        configurer.setBasePackage("org.zhoujunjiang.grade.mapper");
+        return configurer;
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+}
